2be37bb793a89f658e6299697ebe3f6a54a0e46c
Ali Rae <Ali.rae@me.com>
2014-01-04 18:13:58 +0000

    Improved ridgeline finding.

--- src/cpp/ridgelineTools.h
+++ src/cpp/ridgelineTools.h
@@ -1,11 +1,21 @@
 // Filter valid ridge lines from maxima found in 2D wavelet coefficient array.
 
+struct ridgePoint {
+
+        float scale;
+        int col;
+        int lineID;
+
+    };
+
 // Helper function to determine whether we have used this point already. Points can only belong
 // to one ridgeline.
 bool isNewPoint (std::vector<ridgePoint> &ridgeLines, float currentScale, int currentCol) {
 
+    // For the current ridge point, check to see if there is a ridge point close enough to
+    // justify continuing the line.
     for (int i=0;i<ridgeLines.size();i++) {
-        if (ridgeLines[i].scale == currentScale & ridgeLines.col == currentCol) {
+        if (ridgeLines[i].scale == currentScale & ridgeLines[i].col == currentCol) {
             return false;
         }
     }
@@ -18,7 +28,7 @@ bool isNewPoint (std::vector<ridgePoint> &ridgeLines, float currentScale, int cu
 // done by checking that there is a point within 'scaleWindow' and 'colWindow' of the point.
 // These values are currently hard coded, but can be made variable as the search broadens at
 // higher scale factors.
-bool closeEnough (std::vector<ridgePoint> &ridgeLines, float currentScale, float currentCol) {
+bool isCloseEnough (std::vector<ridgePoint> &ridgeLines, float currentScale, float currentCol) {
 
     for (int i=0;i<ridgeLines.size();i++) {
         if (fabs(ridgeLines[i].scale - currentScale) < 3 && fabs(ridgeLines[i].col -
@@ -31,18 +41,19 @@ bool closeEnough (std::vector<ridgePoint> &ridgeLines, float currentScale, float
 
 }
 
-
 // Filter out valid ridgelines
 void filterRidgeLines (std::vector<ridgePoint> &ridgeLines, std::vector<std::vector<float> >
     &maximaArray) {
 
     for (int scale=SCALEMAX;scale>0;scale--) {
         for (int j=0;j<maximaArray[scale].size();j++) {
-            // Check if we have already used this point.
+            // Check if this point already belongs to a ridge line.
             if (isNewPoint(ridgeLines, scale, j)) {
-                if (closeEnough(ridgeLines, scale, j)) {
-                    ridgeLines.push_back({scale,j});
-
+                if (isCloseEnough(ridgeLines, scale, j)) {
+                    ridgePoint point;
+                    point.scale = scale;
+                    point.col = j;
+                    ridgeLines.push_back(point);
                 }
             }
         }
