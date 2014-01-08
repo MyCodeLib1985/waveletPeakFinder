a9a140f63839d1b33f07d80b251e7c10d28cf2dd
Ali Rae <Ali.rae@me.com>
2014-01-08 20:21:56 +0000

    Implemented FFT based convolution.

--- src/cpp/waveletTransform.h
+++ src/cpp/waveletTransform.h
@@ -5,7 +5,6 @@ const float pi = 3.14159265359;
 // Wavelet transform is preformed from a scale factor of 1-SCALEMAX.
 const int SCALEMAX = 30;
 
-std::vector<float> rickerVector (1024,0);
 
 // Populate the vector for the Ricker wavelet at the appropriate scale.
 void rickerArray (std::vector<float> &rickerVector, float scale) {
@@ -37,15 +36,57 @@ void convolution (std::vector<float> &rawData, const std::vector<float> &rickerV
     }
 }
 
+void vectorProduct (std::vector<float> &vector1, std::vector<float> &vector2,
+        std::vector<float> &output) {
+
+    // Check vectors are same length.
+    assert(vector1.size() == vector2.size());
+
+    // Get product of vectors.
+    for (int i=0;i<vector1.size();i++) {
+        output[i] = vector1[i]*vector2[i];
+    }
+}
+
+// Replace with FFT...Common son!
+void dft (std::vector<float> &inputData, std::vector<float> &outputData) {
+
+    // Don't bother calculating complex parts.
+    std::vector<float> cosTerms (inputData.size(),0);
+
+    for (int i=0;i<inputData.size();i++) {
+        for (int j=0;j<inputData.size();j++) {
+            float trigTerm = 2*pi * i * j/inputData.size();
+            cosTerms[i] += inputData[j]*cos(trigTerm);
+            outputData[i] = cosTerms[i];
+        }
+    }
+}
+
 // Wavelet transform function. Currently transforms with fixed Ricker wavelet. Can be easily
 // modified to use various other mother wavelet functions.
 void waveletTransform (std::vector<float> &rawData, std::vector<std::vector<float> >
         &transformedData) {
 
+    std::vector<float> fftOutput (rawData.size(),0);
+
+    std::vector<float> rickerVector (rawData.size(),0);
+
+    // Take fft of the spectral data.
+    dft(rawData,fftOutput);
+
     // Calculate the wavelet transform from scales 1-SCALEMAX
     for (int scale=1;scale<SCALEMAX;scale++) {
         std::cout << scale << std::endl;
         rickerArray(rickerVector,scale);
-        convolution(rawData,rickerVector,transformedData, scale);
+        std::vector<float> fftRickerArray (rawData.size(),0);
+        std::vector<float> output (rawData.size(),0);
+        dft(rickerVector,fftRickerArray);
+        vectorProduct(rickerVector,fftOutput,output);
+        for (int j=0;j<rawData.size();j++) {
+            transformedData[scale][j] = output[j];
+        }
+
     }
 }
+
