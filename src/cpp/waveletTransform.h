bd88b906dedc45802d4b620f8c19e049fb7c5f7b
Ali Rae <Ali.rae@me.com>
2014-01-11 17:31:49 +0000

    Removed CWT implementation.

--- src/cpp/waveletTransform.h
+++ src/cpp/waveletTransform.h
@@ -48,39 +48,3 @@ void vectorProduct (std::vector<float> &vector1, std::vector<float> &vector2,
     }
 }
 
-
-// Wavelet transform function. Currently transforms with fixed Ricker wavelet. Can be easily
-// modified to use various other mother wavelet functions.
-void waveletTransform (std::vector<float> &rawData, std::vector<std::vector<float> >
-        &transformedData) {
-
-    std::vector<float> fftOutput (2*rawData.size(),0);
-
-    // Vector is twice the size of the data set to accommodate padding.
-    std::vector<float> rickerVector (2*rawData.size(),0);
-
-    // Vector to hold padded raw spectral data.
-    std::vector<float> rawDataPadded (2*rawData.size(),0);
-
-    // Pad spectral data
-    padData(rawData,rawDataPadded);
-
-    // Take fft of the padded spectral data.
-    dft(rawDataPadded,fftOutput);
-
-    // Calculate the wavelet transform from scales 1-SCALEMAX
-    for (int scale=1;scale<SCALEMAX;scale++) {
-        std::cout << scale << std::endl;
-        rickerArray(rickerVector,scale);
-        std::vector<float> fftRickerArray (rawDataPadded.size(),0);
-        std::vector<float> output (rawDataPadded.size(),0);
-        std::vector<float> transformedOutput (rawDataPadded.size(),0);
-        dft(rickerVector,fftRickerArray);
-        vectorProduct(fftRickerArray,fftOutput,output);
-        idft(output,transformedOutput);
-        for (int j=0;j<rawDataPadded.size()-rawData.size()/2;j++) {
-            transformedData[scale][j] = transformedOutput[j];
-        }
-    }
-}
-
