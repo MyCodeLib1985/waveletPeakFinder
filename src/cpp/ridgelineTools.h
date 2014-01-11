5dc44f8b11b430eaded41d36046de7c232dfacf7
Ali Rae <Ali.rae@me.com>
2014-01-11 18:04:51 +0000

    Added interface.

--- src/cpp/ridgelineTools.h
+++ src/cpp/ridgelineTools.h
@@ -1,4 +1,13 @@
 #ifndef _RIDGELINETOOLS_
 #define _RIDGELINETOOLS_
 
+void findMaxima (std::vector<std::vector<float> >,
+        std::vector<std::vector<float> >);
+
+void buildRidgeLine (std::vector<ridgePoint>, const
+        std::vector<std::vector<float> >);
+
+void getRidgeLines (std::vector<std::vector<ridgePoint> >, const
+        std::vector<std::vector<float> >);
+
 #endif
