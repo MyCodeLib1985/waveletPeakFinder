e47385b427de35610c2fb24e2bd068b5ef94775b
Ali Rae <Ali.rae@me.com>
2014-01-11 17:21:37 +0000

    Interface and implementation of FFT.

---
+++ src/cpp/fft.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <cmath>
+
+const float pi = 3.14159265359;
+
+void dft(std::vector<float> &inputData, std::vector<float> &outputData) {
+
+    for (int i=0;i<inputData.size();i++) {
+        float temp = 0;
+        for (int j=0;j<inputData.size();j++) {
+            float trigterm = 2*pi * i * j/inputData.size();
+            temp += inputData[j]*cos(trigterm);
+        }
+        outputData[i] = temp;
+    }
+}
+
+void idft (std::vector<float> &inputData, std::vector<float> &outputData) {
+
+    // don't bother calculating complex parts.
+    float norm = 1.0/inputData.size();
+
+    for (int i=0;i<inputData.size();i++) {
+        float temp = 0;
+        for (int j=0;j<inputData.size();j++) {
+            float trigterm = 2*pi * i * j/inputData.size();
+            temp += norm * inputData[j]*cos(trigterm);
+        }
+        outputData[i] = temp;
+    }
+}
+
+void padData(std::vector<float> &input,std::vector<float> &output) {
+
+    // Check if data length is an even number.
+    if (input.size() % 2 != 0) {
+        input.push_back(0);
+    }
+
+    // Pad zeros on either side of data set to 2N
+    for (int i=0;i<input.size()*2;i++) {
+        if (i > input.size()/4 || i < input.size()/2 + input.size()/4) {
+            output[i] = input[i];
+        }
+    }
+}
---
+++ src/cpp/fft.h
@@ -0,0 +1,8 @@
+#ifndef _INCLUDE_FFTH_
+#define _INCLUDE_FFTH_
+
+void dft (const std::vector<float>, std::vector<float>);
+void idft (const std::vector<float>, std::vector<float>);
+void padData(std::vector<float>, std::vector<float>);
+
+#endif
