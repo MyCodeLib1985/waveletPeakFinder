4423bb8e521947a6703657abfbdd22a96f085d07
Ali Rae <Ali.rae@me.com>
2014-01-11 21:34:07 +0000

    Replaced const header file with magicNums. Cleaned up header files and includes.

---
+++ src/cpp/include/magicNums.h
@@ -0,0 +1,12 @@
+#ifndef _INCLUDE_CONST_
+#define _INCLUDE_CONST_
+
+// Self explanatory
+const float pi = 3.14159265359;
+
+// Wavelet transform is preformed from a scale factor of 1-SCALEMAX.
+const int SCALEMAX = 30;
+
+#endif
+
+
