95360b83dfe402374b4a55aaf77ddde29ac8f751
Ali Rae <Ali.rae@me.com>
2014-01-11 17:36:47 +0000

    Removed Ricker wavelet code.

--- src/cpp/waveletTransform.h
+++ src/cpp/waveletTransform.h
@@ -6,21 +6,6 @@ const float pi = 3.14159265359;
 // Wavelet transform is preformed from a scale factor of 1-SCALEMAX.
 const int SCALEMAX = 30;
 
-// Populate the vector for the Ricker wavelet at the appropriate scale.
-void rickerArray (std::vector<float> &rickerVector, float scale) {
-
-    float sigma = 1;
-
-    // Offset to plot ricker wavelet around 0
-    int offset = rickerVector.size()/2;
-
-    for (int i=0;i<rickerVector.size();i++) {
-        float expPart = -1*pow((i-offset)/scale,2)/(2*pow(sigma,2));
-        rickerVector[i] = 2/((sqrt(3*sigma)) * pow(pi,0.25)) * ((1 -
-                (pow((i-offset)/scale,2)/pow(sigma,2))) * exp(expPart));
-    }
-}
-
 // Calculate the value of the wavelet transform at (trans,scale).
 void convolution (std::vector<float> &rawData, const std::vector<float> &rickerVector,
         std::vector<std::vector<float> > &transformedData, float scale) {
