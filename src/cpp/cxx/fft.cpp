699bb5fa166bae7b756b487d3ded9611c9a1078f
Ali Rae <Ali.rae@me.com>
2014-01-12 14:12:27 +0000

    Fixed includes.

--- src/cpp/cxx/fft.cpp
+++ src/cpp/cxx/fft.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cmath>
-#include "include/const.h"
-#include "include/fft.h"
+#include "../include/magicNums.h"
+#include "../include/fft.h"
 
 void dft(std::vector<float> &inputData, std::vector<float> &outputData) {
 
